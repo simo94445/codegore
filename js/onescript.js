b0ab229b999382c682b433b20379e62fe4e2de3d
Simon <simo9445@gmail.com>
2019-01-10 23:31:18 +0100

    Added a calculator, I had to learn how to use the split operator to do this shit, that wasn't in the book bro

--- js/onescript.js
+++ js/onescript.js
@@ -1240,4 +1240,37 @@ accumulator.add( 1 );
 accumulator.add( 10 );
 accumulator.add( 19 ).add( 10 ).add( 40 );
 alert( "The accumulated value is: " + accumulator.read() );
+*/
+
 /*
+function Calculator(){
+    let methods = { // Define an object with our methods for subtraction and addition in an object so that we can add more later
+        "-": ( x, y ) => x - y, // Arrow function expression
+        "+": ( x, y ) => x + y
+    };
+
+    this.calculate = function( str ){ // Create a function using function expression notation with 1 parameter, str.
+
+        let split = str.split(' '), // This splits the string (an array of characters) into 3 parts using whitespaces as seperators (split)
+        x = +split[0], // First number
+        op = split[1], // Mathematical operation
+        y = +split[2]; // Last number
+
+        if( !methods[op] || isNaN(x) || isNaN(y)){ // We only take certain mathematical expressions(op) and numbers (x and y)
+            return NaN;
+        }
+        return methods[op]( x, y ); // Plug in x and y into our methods function, using the chosen operator.
+    }
+        this.addMethod = function(name, func){ // We have to do this to add methods
+        methods[name] = func; // Adds the new function to our methods object.
+    };
+}
+
+let calc = new Calculator;
+calc.addMethod( "*", ( x, y ) => x * y ); // Add our own flavour of mathematical operators
+calc.addMethod( "/", ( x, y ) => x / y );
+calc.addMethod( "**", ( x, y ) => x ** y );
+alert( calc.calculate( "5 + 5" ) ); // Use our sweet new calculator
+alert( calc.calculate( "5 ** 2" ) );
+alert( calc.calculate( "10 / 3" ) );
+*/
