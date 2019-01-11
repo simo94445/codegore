d1ef76cfcc16926c55dbd52f0e8dfa1fc4d88441
Simon <simo9445@gmail.com>
2019-01-11 19:52:04 +0100

    added this file

---
+++ js/meme.js
@@ -0,0 +1,24 @@
+let sindsyg = {
+    tekst(str){
+        this.tekst = str;
+        let sindsygTekst = ""
+        for(let char of str){
+            sindsygTekst += this.random(char);
+        }
+        return sindsygTekst;
+    },
+    random(upperLower){
+        let number = Math.random()*10;
+        if(number>5){
+            return upperLower.toUpperCase();
+            number = Math.random()*10;
+        } else{
+            return upperLower.toLowerCase();
+            number = Math.random()*10;
+        }
+    }
+};
+
+function changeText(){
+  document.getElementById('theText').innerHTML = sindsyg.tekst(document.getElementById('theText').innerHTML);
+}
