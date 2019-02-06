ce66a5747a26e2bf11567d6096c87205addb6daf
Simon <simo9445@gmail.com>
2019-02-06 09:37:13 +0100

    added functions

--- cpp/intro.cpp
+++ cpp/intro.cpp
@@ -4,16 +4,19 @@
 * You can use ready-made functions from the standard library, or you can write them yourself. The main() function is necessary,
 * as it's sort of the 'main program'. */
 
-#include <iostream> // #, number, means that what's coming next is intended for the preprocessor.
-/* You can use #include <filename> to have the preprocessor copy the quoted file to this position in the sourcecode.
+#include <iostream> /* #, number, means that what's coming next is intended for the preprocessor.
+* You can use #include <filename> to have the preprocessor copy the quoted file to this position in the sourcecode.
 * This can be used to include all the information contained in the headerfile. The headerfile iostream comprises conventions
 * for input and output streams (streams being _flows_ of data) */
 
-using namespace  std; // Pre-defined names in C++ can be found in the std (standard) namespace.
-/* The using directive allows direct access to the names of the std namespace.
+using namespace  std; /* Pre-defined names in C++ can be found in the std (standard) namespace.
+* The using directive allows direct access to the names of the std namespace.
 * Namespaces are sets of symbols that are used to organize objects of various kinds, so that these objects may be
 * referred to by their assigned names. Examples of namespaces include file systems (namespaces that assign names to files)
 * computer networks, distributed systems that assign names to resources, such as computers, printers, servers, files, etc. */
+
+
+void line(), foo(), bar(); // Prototypes
 
 /* Programs written in C++ start by running the instructions within the main() function. Apart from the fact that
 * the main() can't be renamed, it's like any other function in C++.*/
@@ -22,7 +25,22 @@ int main(){ // We create a global function with the type Integer.
   cout << "HELLO" << endl; /* The cout function, console output, outputs something to the console.
   * The name cout designates an object responsible for output. The << indicates that something is being pushed to the output-
   stream. endl (end of the line) causes a line feed. */
-
+  line(); // call these functions
+  foo();
+  line();
+  bar();
+  line();
   return 0; /* Here we terminate the main() function returning the value 0 as an exit code to the calling program.
   * In standard practice, exit code 0 means that a program terminated correctly. */
+}
+
+// These functions are just created to be called in the main function, to get an idea of how functions are written.
+void line(){
+  cout << "-------------------------------" << endl;
+}
+void foo(){
+  cout << "foo" << endl;
+}
+void bar(){
+  cout << "bar" << endl;
 }
