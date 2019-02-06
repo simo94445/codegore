f9a67545e6b42eca64edf767fb71510012373bc6
Simon <simo9445@gmail.com>
2019-02-06 08:56:36 +0100

    Sprinkle with C++

---
+++ cpp/intro.cpp
@@ -0,0 +1,14 @@
+/*
+* A C++ program is made up of objects, their accompanying member functions and global functions, which don't belong to any single particular class.
+* Each function fulfills its own designated task, and can also call other functions, and even use these functions outputs as inputs.
+* You can use ready-made functions from the standard library, or you can write them yourself. The main() function is necessary,
+* as it's sort of the 'main program'. 
+*/
+#include <iostream>
+using namespace  std;
+
+int main() // We create a function with the type Integer. main() is a necessary global function, as it's the "main" program.
+{
+  cout << "HELLO" << endl; // The cout function, console output, outputs something to the console.
+  return 0; // Here we return 0, an integer, because our function is of the type integer?
+}
